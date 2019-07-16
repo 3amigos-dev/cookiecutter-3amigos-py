81ae33cafe8b15a2d47473421e4fece727e99778
Tim Gates <tim.gates@iress.com>
2019-07-16 16:30:34 +1000

    Less change of exceeding line length

--- {{cookiecutter.project_slug}}/{{cookiecutter.docker_application_dirname}}/tests/test_main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.docker_application_dirname}}/tests/test_main.py
@@ -5,8 +5,9 @@ Test modules for {{ cookiecutter.py_modulename }}.__main__
 
 def test_main():
     """
-    GIVEN the {{ cookiecutter.py_modulename }}.__main__ module entry point WHEN calling
-    main THEN the call executes successfully with a result of `None`
+    GIVEN the {{ cookiecutter.py_modulename }}.__main__
+    module entry point WHEN calling main THEN the call executes successfully
+    with a result of `None`
     """
     # Setup
     from {{ cookiecutter.py_modulename }}.__main__ import main
