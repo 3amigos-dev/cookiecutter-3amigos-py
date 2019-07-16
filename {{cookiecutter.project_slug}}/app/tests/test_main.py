d63297a4dcfdf126b67d285faff836640abbd860
Tim Gates <tim.gates@iress.com>
2019-07-16 10:38:28 +1000

    More cookie cutter config

--- {{cookiecutter.project_slug}}/app/tests/test_main.py
+++ {{cookiecutter.project_slug}}/app/tests/test_main.py
@@ -1,15 +1,15 @@
 """
-Test modules for pymodulenamegoeshere __main__
+Test modules for {{ cookiecutter.py_modulename }}.__main__
 """
 
 
 def test_main():
     """
-    GIVEN the pymodulenamegoeshere.__main__ module entry point WHEN calling
+    GIVEN the {{ cookiecutter.py_modulename }}.__main__ module entry point WHEN calling
     main THEN the call executes successfully with a result of `None`
     """
     # Setup
-    from pymodulenamegoeshere.__main__ import main
+    from {{ cookiecutter.py_modulename }}.__main__ import main
     # Exercise
     result = main()  # pylint: disable=assignment-from-no-return
     # Verify
