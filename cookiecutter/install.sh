08d82d64218baf86032d9e3e687615df5f389b57
Tim Gates <tim.gates@iress.com>
2019-07-22 15:48:44 +1000

    Add support for shellcheck

--- cookiecutter/install.sh
+++ cookiecutter/install.sh
@@ -6,13 +6,12 @@ BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "${BASEDIR}"
 
-for PYVER in 3.7 ; do
-  cd "${BASEDIR}/pip/${PYVER}"
-  for reqfile in */requirements.txt ; do
-    if [ $(wc -l < "${reqfile}") -gt 0 ] ; then
-      "python${PYVER}" -m pip install -r "${reqfile}"
-    fi
-  done
-  # Display installation
-  "python${PYVER}" -m pip freeze
+PYVER="3.7"
+cd "${BASEDIR}/pip/${PYVER}"
+for reqfile in */requirements.txt ; do
+if [ "$(wc -l < "${reqfile}")" -gt 0 ] ; then
+  "python${PYVER}" -m pip install -r "${reqfile}"
+fi
 done
+# Display installation
+"python${PYVER}" -m pip freeze
