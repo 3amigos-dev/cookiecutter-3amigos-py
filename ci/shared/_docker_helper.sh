e996afe2061f07816e2a22c198f0d4cf58c57706
Tim Gates <tim.gates@iress.com>
2019-07-23 17:27:44 +1000

    Try docker compose run status fix

--- ci/shared/_docker_helper.sh
+++ ci/shared/_docker_helper.sh
@@ -2,8 +2,14 @@
 
 function docker_compose_run() {
     USEROPT="$(id -u):$(id -g)"
+    FAILED=NO
     docker-compose build
     docker-compose up -d
-    docker-compose run --rm -u "${USEROPT}" "$@"
+    if ! docker-compose run --rm -u "${USEROPT}" "$@" ; then
+        FAILED=YES
+    fi
     docker-compose down
+    if [ "${FAILED}" != "NO" ] ; then
+        exit 1
+    fi
 }
