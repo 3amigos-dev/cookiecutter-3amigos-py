5c326f46dbf6fae91be5c73f06725f73cb3dc642
Tim Gates <tim.gates@iress.com>
2019-12-31 21:45:12 +1100

    Add more coverage

--- {{cookiecutter.project_slug}}/{{cookiecutter.docker_application_dirname}}/tests/test_main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.docker_application_dirname}}/tests/test_main.py
@@ -5,11 +5,18 @@ Test modules for:
 """
 
 from click.testing import CliRunner
+import pytest.mark
 
 from {{ cookiecutter.py_modulename }}.__main__ import main
 
 
-def test_main():
+@pytest.mark.parametrize(
+    "args", [
+        ([],),
+        (["invoke"],),
+    ],
+)
+def test_main(args):
     """
     GIVEN the .__main__ module entry point WHEN calling main THEN the call
     executes successfully.
@@ -17,6 +24,6 @@ def test_main():
     # Setup
     runner = CliRunner()
     # Exercise
-    result = runner.invoke(main, [])
+    result = runner.invoke(main, args)
     # Verify
     assert result.exit_code == 0  # nosec
