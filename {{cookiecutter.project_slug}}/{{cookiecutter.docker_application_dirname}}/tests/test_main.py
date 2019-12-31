e3c25a0fb93608a8229cb9e6f0293a9ed51e9225
Tim Gates <tim.gates@iress.com>
2019-12-31 20:56:38 +1100

    Add use of click and click test runner

--- {{cookiecutter.project_slug}}/{{cookiecutter.docker_application_dirname}}/tests/test_main.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.docker_application_dirname}}/tests/test_main.py
@@ -4,16 +4,19 @@ Test modules for:
 {{ cookiecutter.py_modulename }}.__main__
 """
 
+from click.testing import CliRunner
+
 from {{ cookiecutter.py_modulename }}.__main__ import main
 
 
 def test_main():
     """
     GIVEN the .__main__ module entry point WHEN calling main THEN the call
-    executes successfully with a result of `None`
+    executes successfully.
     """
     # Setup
+    runner = CliRunner()
     # Exercise
-    result = main()  # pylint: disable=assignment-from-no-return
+    runner.invoke(main, [])
     # Verify
-    assert result is None  # nosec
+    assert result.exit_code == 0  # nosec
